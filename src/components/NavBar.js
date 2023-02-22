3566bed563c77237f5aa42eb8db328eea84783bf
Abby <abby.e.smith.85@gmail.com>
2023-02-22 13:33:28 -0700

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
-}
-
+  return( 
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>
+)}
+//Why is the href = #home?
 export default NavBar;
